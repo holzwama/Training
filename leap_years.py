838d01a14fbfe6df6ca7ee26a64424912db332fa
holzwama <80957330+holzwama@users.noreply.github.com>
2021-03-30 13:03:55 +0200

    Add files via upload

--- leap_years.py
+++ leap_years.py
@@ -8,15 +8,16 @@ list_year = []
 def fill_list(empty_list):
     counter = 1
     while len(empty_list) != 20:
-        list_year.append(users_year + counter)
+        empty_list.append(users_year + counter)
         counter += 1
 
 def leap_year(list_):
+    
     for x in list_:
         if x % 100 == 0:
             print(x, "No leap year")
         elif x % 400 == 0:
-             print(x, "is a leap year")
+            print(x, "is a leap year")
         elif x %   4 == 0:
             print(x, "Is a leap year")
         else:
