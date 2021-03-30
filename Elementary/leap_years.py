a0ad2c0f867b6d6a9df274f697a5d8041567ad40
mgrum <mgrum1994@gmail.com>
2021-03-30 13:39:06 +0200

    Folders updated

---
+++ Elementary/leap_years.py
@@ -0,0 +1,27 @@
+
+next_years = 20
+print("Type a year")
+users_year = int(input())
+
+list_year = []
+
+def fill_list(empty_list):
+    counter = 1
+    while len(empty_list) != 20:
+        empty_list.append(users_year + counter)
+        counter += 1
+
+def leap_year(list_):
+    
+    for x in list_:
+        if x % 100 == 0:
+            print(x, "No leap year")
+        elif x % 400 == 0:
+            print(x, "is a leap year")
+        elif x %   4 == 0:
+            print(x, "Is a leap year")
+        else:
+            print(x, "No leap year")
+
+fill_list(list_year)
+leap_year(list_year)
