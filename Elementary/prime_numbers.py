ec44483836aafbe0ebce64970770f987db5153ca
Marvin <holzawrthu@web.de>
2021-04-07 10:49:19 +0200

    fixxed

--- Elementary/prime_numbers.py
+++ Elementary/prime_numbers.py
@@ -1,17 +1,19 @@
-max_value = int(input("Maximalwert eingeben, bis zu welchem die Primzahlen ausgegeben werden sollen: "))
-print(max_value)
+def primes():
+    n = 1
+    prim = True
+    max_value = int(input("Maximalwert eingeben, bis zu welchem die Primzahlen ausgegeben werden sollen: "))
+    while n < max_value:
+        prim = True
+        if n == 1:
+              prim = False      
+        else:
+            i = 2
+            while i <= n - 1:
+                if n % i == 0:
+                    prim = False
+                i += 1
+        if prim == True:
+            print(n)
+        n += 1
 
-list_numbers = range(1, max_value+1)
-
-
-
-def primes(prime_list):
-   
-    for x in prime_list:
-        if x % 1 == 0:
-            print(x, "ist keine Primzahl")            
-        elif x % x == 0:
-            print(x,"ist eine Primzahl")
-            
-
-primes(list_numbers)
+primes()
