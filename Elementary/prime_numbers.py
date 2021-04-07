3b07dbacdb0e68d738cc42187f78206cd9ea542f
mgrum <mgrum1994@gmail.com>
2021-04-07 14:06:29 +0200

    Marcel 07.04.2021

--- Elementary/prime_numbers.py
+++ Elementary/prime_numbers.py
@@ -1,19 +1,32 @@
-def primes():
-    n = 1
+def primes(max_value=100):
+    start = 2
     prim = True
-    max_value = int(input("Maximalwert eingeben, bis zu welchem die Primzahlen ausgegeben werden sollen: "))
-    while n < max_value:
+    primes = []
+    for n in range(start, max_value+1):
         prim = True
-        if n == 1:
-              prim = False      
-        else:
-            i = 2
-            while i <= n - 1:
-                if n % i == 0:
-                    prim = False
-                i += 1
+        for i in range(2, n):
+            if n % i == 0:
+                prim = False
         if prim == True:
-            print(n)
-        n += 1
+            primes.append(n)
+    return primes
 
-primes()
+def primes2(n):
+    """ Returns  a list of primes < n """
+    # Stichwort Sieb des Erastothenes
+    # https://www.youtube.com/watch?v=Cg8P4GNPe0I
+    sieve = [True] * n
+    for i in range(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
+    return [2] + [i for i in range(3,n,2) if sieve[i]]
+
+def main():
+    max_value = int(input("Maximalwert eingeben, bis zu welchem die Primzahlen ausgegeben werden sollen: "))
+    my_primes = primes(max_value)
+    print(my_primes)
+    # print(primes(1000))
+    # primes2(1000000)
+
+if __name__ == "__main__":
+    main()
--- Sorting/bubblesort.py
+++ Sorting/bubblesort.py
@@ -1,20 +1,23 @@
 import random
-LIST_SIZE = 10
-MAX = 10
+
+LIST_SIZE = 20
+MAX = 100
 MIN = 0
-random_list = [random.randint(MIN,MAX) for i in range(LIST_SIZE)]
+
 
 def bubblesort(lis):
-
     size = len(lis)
     
     if size < 2:
         return lis
 
-    for j in range(len(lis)):
+    for _ in range(len(lis)):
         for i in range(len(lis) - 1):
             if lis[i] > lis[i +1]:
-                lis[i], lis[i +1] = lis[j+1], lis[j]
+                lis[i], lis[i +1] = lis[i+1], lis[i]
+
+
+random_list = [random.randint(MIN,MAX) for i in range(LIST_SIZE)]
 print(random_list)
 bubblesort(random_list)
 print(random_list)
