091f4c151125dada9b35f0a465c3f70f7c1b6625
Marvin <holzawrthu@web.de>
2021-04-16 14:54:29 +0200

    New

--- Intermediate/intermediate_01.py
+++ Intermediate/intermediate_01.py
@@ -1,27 +1,11 @@
 numbers_list = []
-max_value = 100 + 1
+max_value = 9 
 list_year = []
 def fill_list(empty_list):
-    counter = 0
+    counter = 1
     while len(empty_list) != max_value:
         empty_list.append(counter)
         counter += 1
 
-
-def until_100(list_):
-    result = 0
-    count_one = 0
-    count_two = 1
-    temp = 0
-    while result != 100:
-        temp = list_[count_one] + list_[count_two]
-        result = result + temp
-        count_one += 1
-        count_two += 1
-        print(result)
-
-
-
 fill_list(numbers_list)
-until_100(numbers_list)
-
+print(numbers_list)
---
+++ Übungsblatt_02/Aufgabe_02.py
@@ -0,0 +1,3 @@
+Name = input(print("Geben Sie ihren Vornamen an"))
+
+print((Name[:1]) + "x" + Name[-4:])
