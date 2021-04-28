1ab40859515d076b6ec9c2b69878a6eed1c7459e
Marvin <holzawrthu@web.de>
2021-04-28 18:05:52 +0200

    ggt funktion added

--- Übungsblatt_03/Aufgabe_02.py
+++ Übungsblatt_03/Aufgabe_02.py
@@ -1,20 +1,23 @@
 
 def teiler(num_one, num_two):
-    counter = 1
+    counter = 0
+    big_num = num_one
     lis_one = []
     lis_two = []
-    while counter <= num_one:
+    if big_num <= num_two:
+        big_num = num_two
+    while counter < big_num:
+        counter +=1
         if num_one % counter == 0:
-            lis_one.append(counter)
+             lis_one.append(counter)
         if num_two % counter == 0:
-            lis_two.append(counter)
-        else:
-            counter +=1
-    print(lis_one)
+            lis_two.append(counter) 
+    print(set(lis_one).intersection(lis_two)) # Vergleicht die Werte von zwei Listen miteinander & gibt diese aus
+
 def main():
     print("Geben Sie zwei Zahlen ein, um den größten Gemeinsamen Teiler zu berechnen")
-    x = print(input("Zahl 1: \n"))
-    y = print(input("Zahl 2: \n"))
+    x = int(input("Zahl 1: \n"))
+    y = int(input("Zahl 2: \n"))
     teiler(x,y)
     
 if __name__ == "__main__":
--- Übungsblatt_03/test.py
+++
@@ -1,8 +0,0 @@
-counter = 1
-lis_one = []
-x = 20
-while counter <= 20:
-    if x % counter == 0:
-        lis_one.append(counter)
-        counter += 1
-print (lis_one)
