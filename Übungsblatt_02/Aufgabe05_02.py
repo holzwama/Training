9560a54d6351f096ae04153654e39d4123736a01
Marvin Holzwarth <HolzwaM2@basfad.basf.net>
2021-05-28 09:50:11 +0200

    ok

---
+++ Übungsblatt_02/Aufgabe05_02.py
@@ -0,0 +1,3 @@
+de2en = {"eins" : "one", "zwei" : "two" , "drei" : "three", 1 : "one", 2 : "two" , 3 : "three"}
+
+
---
+++ Übungsblatt_03/Aufgabe_02_alternative.py
@@ -0,0 +1,11 @@
+# Iterative Funktion ggT mit 2 Parametern x | y
+# X | Y sortieren nach Größen -> X > Y
+# while ggT = 0
+# if x % y == 0 then ggT = y
+# else x,y = y, x % y
+
+#It. Funktion ggTL die den größten ggT von einer Liste berechnet
+# Jeden Teiler jeder Zahl herausfinden
+# result = ggT(erste Zahl, zweite Zahl)
+# for next in lis[2:]
+#   result = ggT(result, next)
