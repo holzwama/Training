204d810beff672fd8be24faab2f6d6a581598e5f
Marvin Holzwarth <HolzwaM2@basfad.basf.net>
2021-06-15 14:06:15 +0200

    Aufgaben added

--- Übungsblatt_04/Aufgabe_01.py
+++ Übungsblatt_04/Aufgabe_01.py
@@ -1,9 +1,47 @@
 ##Kubikzahlen
-basic_numbers = [1,2,3,4,5,6,7,8,9,10]
-kubik_list = [x*x*x for x in basic_numbers]
+print("GERADE KUBIKZAHLEN")
+basic_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+kubik_list = [x ** 3 for x in basic_numbers if x ** 3 % 2 == 0]
+kubik_list_a = list(filter(lambda y: y % 2 == 0, map(lambda x: x ** 3, basic_numbers)))
 print(kubik_list)
+print(kubik_list_a)
 
 ##Teiler
+print("ALLE TEILER / List Comprehension")
 
-print(basic_numbers)
 
+def teilerVonZ(z):
+    return [x for x in range(2, z // 2 + 1) if z % x == 0]
+
+
+print(teilerVonZ(123))
+print(teilerVonZ(12345))
+print(teilerVonZ(123456))
+
+print("ALLE TEILER / primitive")
+
+
+def funTeilerVonZ(z):
+    return list(filter(lambda x: z % x == 0, range(2, z // 2 + 1)))
+
+
+print(funTeilerVonZ(123))
+print(funTeilerVonZ(12345))
+print(funTeilerVonZ(123456))
+
+##Primzahlen
+print("Primzahlen")
+
+
+def checkPrime(n):
+    for x in range(2, n // 2 + 1):
+        if n % x == 0:
+            return False
+    return True
+
+
+primzahlen = [x for x in range(10000, 10100 + 1) if checkPrime(x)]
+primzahlen_a = list(filter(lambda x: checkPrime(x), range(10000, 10100 + 1)))
+
+print(primzahlen)
+print(primzahlen_a)
---
+++ Übungsblatt_04/Aufgabe_02.py
@@ -0,0 +1,7 @@
+# 1. 4 Zahlen
+# 2. Müssen alle unterschiedlich sein (1,3,4)
+# 3. Kubikzahlen von diesen berechnen
+# 4. a^3 + b^3 = c^3 + d^3 
+# 5. keine doppelten Ergebnisse
+# Ergebniss soll aussehen wie folgt: [(1729, 1,12,9,10), (????, ?,?,?,?)] -> Tipp: sortieren
+#
