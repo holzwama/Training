7434cb7585d27c00d2047b4de43b1a7e87f434cb
Marvin Holzwarth <HolzwaM2@basfad.basf.net>
2021-06-29 16:41:27 +0200

    test

--- Übungsblatt_04/Aufgabe_03.py
+++ Übungsblatt_04/Aufgabe_03.py
@@ -3,3 +3,12 @@ Buchstaben = ["a","b","c"]
 print(list(zip(Zahlen, Buchstaben)))
 
 print(list(zip((1,2,3,4), (5,6), (7,8))))
+
+def unzip(lis):
+    return list(zip(*lis))
+
+zipped = list(zip((1,2,3), (4,5), (6,7,8)))
+unzipped = list(unzip(zipped))
+
+print(zipped)
+print(unzipped)
