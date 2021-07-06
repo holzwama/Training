330952cde34cb994a0c27107ecef0aceac64a8ac
Marvin <holzawrthu@web.de>
2021-07-06 12:50:43 +0200

    WC added

--- Übungsblatt_05/WC.py
+++ Übungsblatt_05/WC.py
@@ -1,12 +1,48 @@
-from pathlib import Path
+# funktion um Anzahl der Wörter zu bestimmen
+def wordsFunc(userlang):
+    with open('Test_python.txt') as infile:
+        words=0
+        for line in infile:
+            wordslist=line.split()
+            words=words+len(wordslist)
+    if userlang == "de":
+        print(words, "Wörter")
+    else:
+        print(words, "words")
 
-file = open("Test_python.txt","rt")
+# funktion um Anzahl der Buchstaben zu bestimmen
+def charsFunc(userlang):
+    with open('Test_python.txt') as infile:
+        words=0
+        characters=0
+        for line in infile:
+            wordslist=line.split()
 
-def pathname(path):
-    path_name = Path(path).stem
-    return path_name
+            words=words+len(wordslist)
+            characters += sum(len(Word) for Word in wordslist)
+    if userlang == "de":
+        print(characters, "Buchstaben")
+    else:
+        print(characters, "letters")
+
+# funktion um Anzahl der Linien zu bestimmen
+def linesFunc(userlang):
+    with open('Test_python.txt') as infile:
+        lines=0
+        for line in infile:
+            lines=lines+1
+    if userlang == "de":
+        print(lines, "Linien")
+    else:
+        print(lines, "lines")
+        
 
 
-print(file.read())
+def main():
+    lang = input("Welche Sprache möchten Sie ausgegeben bekommen? (de | en) \n")
+    linesFunc(lang)
+    wordsFunc(lang)
+    charsFunc(lang)
 
-file.close()
+if __name__ == "__main__":
+    main()
