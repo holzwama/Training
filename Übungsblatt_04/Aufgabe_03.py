ac23eb75f7bb8cab454e712f65be7eaa05083844
Marvin Holzwarth <HolzwaM2@basfad.basf.net>
2021-07-07 11:41:46 +0200

    test

--- Übungsblatt_04/Aufgabe_03.py
+++ Übungsblatt_04/Aufgabe_03.py
@@ -1,14 +1,13 @@
-Zahlen = [1,2,3,4]
-Buchstaben = ["a","b","c"]
-print(list(zip(Zahlen, Buchstaben)))
+#Zahlen = [1,2,3,4]
+#Buchstaben = ["a","b","c"]
+#print(list(zip(Zahlen, Buchstaben)))
 
-print(list(zip((1,2,3,4), (5,6), (7,8))))
+#print(list(zip((1,2,3,4), (5,6), (7,8))))
 
 def unzip(lis):
     return list(zip(*lis))
 
-zipped = list(zip((1,2,3), (4,5), (6,7,8)))
-unzipped = list(unzip(zipped))
 
-print(zipped)
-print(unzipped)
+print(list(unzip(list(zip((1,2,3,4), (5,6), (7,8))))) == [(1,2), (5,6), (7,8)])
+t = [(1,2), (3,4), (5,6)]
+print(list(unzip(list(unzip(t)))) == t)
--- Übungsblatt_04/Aufgabe_04.py
+++ Übungsblatt_04/Aufgabe_04.py
@@ -1,35 +1,35 @@
 import string
 
-buchstabenListeLower = list(string.ascii_lowercase)
-buchstabenListeUpper = list(string.ascii_uppercase)
+CHARSLOWER = list(string.ascii_lowercase)
+CHARSUPPER = list(string.ascii_uppercase)
+
+def rot13(text):
+    result = '' 
+
+    for letter in text:
+        #Kleinbuchstaben herausfinden
+        if letter.islower() & letter.isalpha():
+            text = text.lower()
+            result += CHARSLOWER[(CHARSLOWER.index(letter) + 13) % 26]
+        ##Großbuchstaben herausfinden
+        elif letter.isupper() & letter.isalpha():
+            text = text.upper()
+            result += CHARSUPPER[(CHARSUPPER.index(letter) + 13) % 26]
+
+    return result
+
+def saveFile(text):
+    f = open("Test_swap.txt", "w")
+    f.write(text)
+    f.close()
+
 try:
     userinput = input('"Welche Datei wollen sie öffnen?"')
-
     with open(userinput, 'r') as f2:
         data = f2.read()
+    encData = rot13(data)
+    print(encData)
+    saveFile(encData)
 
-
-    def rot13(text):
-        result = '' 
-
-        for letter in text:
-            #Kleinbuchstaben herausfinden
-            if letter.islower() & letter.isalpha():
-                text = text.lower()
-                result += buchstabenListeLower[(buchstabenListeLower.index(letter) + 13) % 26]
-            ##roßbuchstaben herausfinden
-            elif letter.isupper() & letter.isalpha():
-                text = text.upper()
-                result += buchstabenListeUpper[(buchstabenListeUpper.index(letter) + 13) % 26]
-    
-        return result
-
-    def saveFile(text):
-       f = open("Test_swap.txt", "w")
-       f.write(text)
-       f.close()
-
-    print(rot13(data))
-    saveFile(rot13(data))
 except FileNotFoundError:
     print("Falscher Dateiname")
